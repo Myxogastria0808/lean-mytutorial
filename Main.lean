2b03076d06093b2e9496a913d8699806ff9efdb6
Myxogastria0808 <r.rstudio.c@gmail.com>
2026-03-02 06:19:58 +0900

    wip

--- Main.lean
+++ Main.lean
@@ -1,22 +1,21 @@
-import «lean-mytutorial».OneEqualOne
-import «lean-mytutorial».Total
-import «lean-mytutorial».Theory
-import «lean-mytutorial».DependentType
+-- import «lean-mytutorial».sample.OneEqualOne
+-- import «lean-mytutorial».sample.Total
+-- import «lean-mytutorial».sample.Theory
 
--- OneEqualOne.lean
-#check n
-#check one_equal_one
-#check twice
-#check twice'
+-- -- OneEqualOne.lean
+-- #check n
+-- #check one_equal_one
+-- #check twice
+-- #check twice'
 
--- Total.lean
-#check total
+-- -- Total.lean
+-- #check total
 
--- Theory.lean
-#check A
-#check B
+-- -- Theory.lean
+-- #check A
+-- #check B
 
--- DependentType.lean
+import «lean-mytutorial».tutorial.DependentType
 
 def main : IO Unit :=
   IO.println s!"Hello, world! (Entry Point)"
